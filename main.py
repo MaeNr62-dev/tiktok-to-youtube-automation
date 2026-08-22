9d0fd022e0d7bc23da55a49fcfefa5de5a79d228
Mae.Nr62 <150275442+MaeNr62-dev@users.noreply.github.com>
2026-08-22 15:50:55 +0200

    Update TikTok username in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from logger import setup_logger
 if __name__ == "__main__":
     setup_logger()
     try:
-        TIKTOK_USERNAME = "your_tiktok_username"  # Replace with your TikTok username
+        TIKTOK_USERNAME = "mae.nr62"  # Replace with your TikTok username
         DOWNLOAD_DIR = "./tiktok_downloads"
         download_tiktok_clips(TIKTOK_USERNAME, DOWNLOAD_DIR)
         process_and_upload_clips(DOWNLOAD_DIR)
